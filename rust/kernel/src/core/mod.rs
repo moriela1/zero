cbde4f9398b8f5c90438b6bffa34d162247a930d
dywoq <aleks.koyf@gmail.com>
2026-01-30 21:54:14 +0300

    rust, tools: Apply copyright notice

--- rust/kernel/src/core/mod.rs
+++ rust/kernel/src/core/mod.rs
@@ -1,3 +1,11 @@
+// Copyright (c) 2026 dywoq
+//
+// This code is released under the Apache License 2.0:
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Repository:
+// https://github.com/dywoq/zero
+
 pub fn entry() {
     loop {}
 }
