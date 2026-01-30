34b6e0f17455dcfe1b113d2baaf046e6a8c65ef0
dywoq <aleks.koyf@gmail.com>
2026-01-30 23:10:13 +0300

    tools/zerocli: Add internal/cmd package

--- rust/kernel/src/core/mod.rs
+++ rust/kernel/src/core/mod.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2026 dywoq
+// Copyright 2026 dywoq
 //
 // This code is released under the Apache License 2.0:
 // https://www.apache.org/licenses/LICENSE-2.0
--- tools/zerocli/main.go
+++ tools/zerocli/main.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2026 dywoq
+// Copyright 2026 dywoq
 //
 // This code is released under the Apache License 2.0:
 // https://www.apache.org/licenses/LICENSE-2.0
@@ -8,6 +8,25 @@
 
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/dywoq/zero/tools/zerocli/internal/cmd"
+)
+
 func main() {
+	cmd := cmd.Parser{}
 	
+	file, err := os.Open("help.toml")
+	if err != nil {
+		panic(err)
+	}
+	
+	conf, err := cmd.Parse(file)
+	if err != nil {
+		panic(err)
+	}
+	
+	fmt.Printf("conf: %v\n", conf)
 }
