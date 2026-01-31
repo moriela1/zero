bdc738d59853017183a45fe4d3a73f9e74873e91
dywoq <aleks.koyf@gmail.com>
2026-01-31 10:26:43 +0300

    tools/zerocli: Remove internal package

--- tools/zerocli/main.go
+++ tools/zerocli/main.go
@@ -10,12 +10,7 @@ package main
 
 import (
 	"fmt"
-	"io/fs"
 	"os"
-	"path/filepath"
-
-	"github.com/dywoq/zero/tools/zerocli/internal/cmd"
-	"github.com/dywoq/zero/tools/zerocli/internal/root"
 )
 
 // die Checks if err is not nil.
@@ -28,37 +23,5 @@ func die(err error, format string, v ...any) {
 }
 
 func main() {
-	// Search for root configuration first.
-	rootFile, err := os.Open("zerocli.toml")
-	die(err, "Failed to open zerocli.toml configuration file: %v", err)
 
-	rootParser := root.ConfigParser{}
-	config, err := rootParser.Parse(rootFile)
-	die(err, "Failed to parse zerocli.toml configuration file: %v", err)
-
-	cmdParser := cmd.ConfigParser{}
-
-	// Search for command configurations
-	configs := []*cmd.Config{}
-	err = filepath.WalkDir(config.CommandsDir, func(path string, d fs.DirEntry, err error) error {
-		if d.IsDir() {
-			return nil
-		}
-
-		ext := filepath.Ext(d.Name())
-		if ext == ".toml" {
-			f, err := os.Open(path)
-			if err != nil {
-				return err
-			}
-			config, err := cmdParser.Parse(f)
-			if err != nil {
-				return err
-			}
-			configs = append(configs, config)
-		}
-
-		return nil
-	})
-	die(err, "Failed to walk through \"%s\" directory: %s", config.CommandsDir, err)
 }
