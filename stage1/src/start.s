0e3b781795588f74d21fba699d1989890303a9eb
dywoq <aleks.koyf@gmail.com>
2026-02-09 19:44:48 +0300

    stage0, stage1: Add copyright notice

--- stage1/src/start.s
+++ stage1/src/start.s
@@ -1,3 +1,11 @@
+; Copyright 2026 dywoq
+; 
+; This code is published under Apache License 2.0:
+; http://www.apache.org/licenses/LICENSE-2.0
+; 
+; The repository:
+; https://github.com/dywoq/zero
+
 bits 16
 org 0x9000
 
--- stage1/src/video.s
+++ stage1/src/video.s
@@ -1,3 +1,11 @@
+; Copyright 2026 dywoq
+; 
+; This code is published under Apache License 2.0:
+; http://www.apache.org/licenses/LICENSE-2.0
+; 
+; The repository:
+; https://github.com/dywoq/zero
+
 %ifndef STAGE1_VIDEO_S
 %define STAGE1_VIDEO_S
 
