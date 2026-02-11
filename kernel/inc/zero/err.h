b21ec9d681c1f6d7f8b852b605d213633692824b
dywoq <aleks.koyf@gmail.com>
2026-02-11 08:46:19 +0300

    kernel: err.h: Add enum zero_err

---
+++ kernel/inc/zero/err.h
@@ -0,0 +1,20 @@
+// Copyright 2026 dywoq
+//
+// This code is published under Apache License 2.0:
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// The repository:
+// https://github.com/dywoq/zero
+
+#ifndef _ZERO_ERR_H
+#define _ZERO_ERR_H
+
+#include <types.h>
+
+// A error enumeration with codes.
+// It's the main way to report failures in functions.
+enum zero_err : ushort_t {
+    ZERO_ERR_OK = 0,
+};
+
+#endif
